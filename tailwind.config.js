a0800929e26e6a2f6078860cd98b126e9f79eb5f
Kamil Kominek <kamil.kominek@makimo.pl>
2022-02-24 22:00:36 +0100

    Add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,14 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
 };
