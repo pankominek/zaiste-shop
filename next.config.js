e01202131c6143496c658983470daa4e49b6012c
Kamil Kominek <kamil.kominek@makimo.pl>
2022-03-05 17:51:45 +0100

    Add products-csr to homepage and change url API

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
-    domains: ['fakestoreapi.com'],
+    domains: ['naszsklep-api.vercel.app'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,74 @@
-const DATA = {
-  title: "Home",
-  description: "Lorem ipsum dolor sit amet.",
+import { useQuery } from "react-query";
+import { ProductDetails } from "../components/Product";
+
+const getProducts = async () => {
+  const res = await fetch(
+    `https://naszsklep-api.vercel.app/api/products`
+  );
+  const data: StoreApiResponse[] = await res.json();
+  return data;
 };
 
 const Home = () => {
+  const { isLoading, data, error } = useQuery("products", getProducts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || error) {
+    return <div>No data</div>;
+  }
+
   return (
     <>
-      <h2 className="font-semibold">{DATA.title}</h2>
-      <p>{DATA.description}</p>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {data.map((product) => {
+          return (
+            <li key={product.id} className="shadow border-2">
+              <ProductDetails
+                data={{
+                  id: product.id,
+                  title: product.title,
+                  description: product.description,
+                  thumbnailUrl: product.image,
+                  thumbnailAlt: product.title,
+                  rating: product.rating.rate,
+                }}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://naszsklep-api.vercel.app/api/products`
+  );
+  const data: StoreApiResponse[] = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+interface StoreApiResponse {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  longDescription: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
