99654d863076ff97c90bcae3d6a8162cb65b691c
Kamil Kominek <kamil.kominek@makimo.pl>
2022-04-21 15:44:38 +0200

    Add mutation integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,34 @@
+import { useCreateProductReviewMutation } from '../generated/graphql';
+
 const Home = () => {
+  const [createReview, { data, loading, error }] = useCreateProductReviewMutation();
+
+  const addReview = () =>
+    createReview({
+      variables: {
+        review: {
+          "headline": "Lorem ipsum",
+          "name": "Kamil",
+          "email": "kamil@pankominek.pl",
+          "content": "Bardzo dobry produkt!",
+          "rating": 5
+        }
+      }
+    });
+
   return (
-    <p>Homepage</p>
+    <>
+      <button onClick={addReview} type="button">Dodaj komentarz</button>
+      {loading && (
+        <span>loading..</span>
+      )}
+      {error && (
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+      )}
+      {data && (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+    </>
   );
 };
 
